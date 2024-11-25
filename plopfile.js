3232a1eb1b24f58cb40739e11ade98333acd3b6f
watorutart <watoru374215@gmail.com>
2024-11-26 02:32:56 +0900

    refactor: :recycle: Prettierの設定を追加、いくつかのファイルをフォーマット

--- plopfile.js
+++ plopfile.js
@@ -5,6 +5,7 @@ module.exports = function (
   plop.setGenerator('component', {
     description: 'Create a new component',
     prompts: [
+      // npx plop実行時のプロンプト設定
       {
         type: 'input',
         name: 'path',
@@ -17,21 +18,22 @@ module.exports = function (
       },
     ],
     actions: [
+      // 実行する操作の設定
       {
         type: 'add',
         path: './{{path}}/{{pascalCase name}}/index.tsx',
-        templateFile: 'plop-templates/componentIndex.tsx.hbs'
+        templateFile: 'plop-templates/componentIndex.tsx.hbs',
       },
       {
         type: 'add',
         path: './{{path}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
-        templateFile: 'plop-templates/component.tsx.hbs'
+        templateFile: 'plop-templates/component.tsx.hbs',
       },
       {
         type: 'add',
         path: './{{path}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
-        templateFile: 'plop-templates/component.stories.tsx.hbs'
-      }
+        templateFile: 'plop-templates/component.stories.tsx.hbs',
+      },
     ],
   });
-}
+};
